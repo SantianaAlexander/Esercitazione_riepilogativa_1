c351cbd87adc9caea94574626e78e939b18def12
Alexander Santiana <santiana.a@iisgalvanimi.edu.it>
2025-01-22 19:04:13 +0000

    Finito tutto

--- app.py
+++ app.py
@@ -2,37 +2,61 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from models import Utenti
+from models import Utenti, db
 
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///utenti.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'supersecretkey'
+
 db.init_app(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
 with app.app_context():
     db.create_all()
 
-login_manager = LoginManager()
-login_manager.init_app(app) 
-login_manager.login_view = 'login'
-
-from flask import Flask
-app = Flask(__name__)
+@login_manager.user_loader
+def load_user(user_id):
+    return Utenti.query.get(int(user_id))
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request-methhod == 'POST':
+    if request.method == 'POST':
         username = request.form['username']
         hashed_password = request.form['password']
-        pw_hash = bcrypt.generate_password_hash(hashed_password, 10)
-        if Utenti.query.filter_by(username = username).fisrt():
+        pw_hash = bcrypt.generate_password_hash(hashed_password, 10).decode('utf-8')
+        if Utenti.query.filter_by(username = username).first():
             return render_template('register.html', error="Questo username è già in uso. Ritenta!")
-        new_user = User(username=username, hashed_password=pw_hash)
+        new_user = Utenti(username=username, hashed_password=pw_hash)
         db.session.add(new_user)
         db.session.commit()
-        return redirect(url_for('login'))
+        login_user(new_user)
+        return redirect(url_for('home'))
     return render_template('register.html', error=None)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = Utenti.query.filter_by(username=username).first()
+        if user and bcrypt.check_password_hash(user.hashed_password, password):
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            return render_template('login.html', error="Credenziali non valide.")
+    return render_template('login.html', error=None)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
--- models.py
+++ models.py
@@ -4,6 +4,6 @@ from flask_login import UserMixin
 db = SQLAlchemy()
 
 class Utenti(db.Model, UserMixin):
-    id_user = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False)
-    hashed_password(db.String(100), nullable=False)
+    hashed_password = db.Column(db.String(100), nullable=False)
